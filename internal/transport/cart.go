5fdd173b181d9b20f10eeb87f540dae9146f6805
Dmitry Leschik <dmitry.leschik@gmail.com>
2024-05-27 18:59:44 +0300

    added transport layer

--- internal/transport/cart.go
+++ internal/transport/cart.go
@@ -2,8 +2,11 @@ package transport
 
 import (
 	"encoding/json"
+	"errors"
 	"net/http"
+	"strconv"
 
+	"github.com/p1mako/cart-api/internal/models"
 	"github.com/p1mako/cart-api/internal/services"
 )
 
@@ -34,13 +37,94 @@ func (c *CartHandler) Create(w http.ResponseWriter, _ *http.Request) {
 }
 
 func (c *CartHandler) AddItem(w http.ResponseWriter, r *http.Request) {
+	var item models.CartItem
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	cart := models.Cart{Id: id}
+	err = json.NewDecoder(r.Body).Decode(&item)
+	item.CartId = id
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	cart, err = c.service.AddItem(cart, item)
+	if errors.Is(err, services.ErrNoProductName) || errors.Is(err, services.ErrBadQuantity) || errors.Is(err, new(services.ErrNoSuchCart)) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	marshaledCart, err := json.Marshal(cart)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(marshaledCart)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func (c *CartHandler) RemoveItem(w http.ResponseWriter, r *http.Request) {
-
+	id, err := strconv.Atoi(r.PathValue("id"))
+	itemId, err := strconv.Atoi(r.PathValue("item_id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	cart := models.Cart{Id: id}
+	item := models.CartItem{Id: itemId, CartId: id}
+	cart, err = c.service.RemoveItem(cart, item)
+	if errors.Is(err, new(services.ErrNoSuchCart)) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	marshaledCart, err := json.Marshal(cart)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(marshaledCart)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
 
 func (c *CartHandler) View(w http.ResponseWriter, r *http.Request) {
-
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	cart := models.Cart{Id: id}
+	result, err := c.service.GetCart(cart.Id)
+	if errors.Is(err, new(services.ErrNoSuchCart)) {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	marshaled, err := json.Marshal(result)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	_, err = w.Write(marshaled)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 }
