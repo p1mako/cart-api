4f2bfdc1825d3424292ce2a87ffc35260de03207
Dmitry Leschik <dmitry.leschik@gmail.com>
2024-05-29 21:26:11 +0300

    add mock for CartItemDb

--- internal/services/cartitem_test.go
+++ internal/services/cartitem_test.go
@@ -1,1 +1,23 @@
 package services
+
+import (
+	"github.com/p1mako/cart-api/internal/models"
+	"github.com/stretchr/testify/mock"
+)
+
+type CartItemDbMock struct {
+	mock.Mock
+}
+
+func (db *CartItemDbMock) Create(items ...models.CartItem) ([]models.CartItem, error) {
+	args := db.Called(items)
+	return args.Get(0).([]models.CartItem), args.Error(1)
+}
+func (db *CartItemDbMock) LoadCartItems(cart int) ([]models.CartItem, error) {
+	args := db.Called(cart)
+	return args.Get(0).([]models.CartItem), args.Error(1)
+}
+func (db *CartItemDbMock) Remove(item models.CartItem) (int, error) {
+	args := db.Called(item)
+	return args.Int(0), args.Error(1)
+}
