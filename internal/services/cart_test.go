d5cdf4f26623e1e7dde34483a41e8567cca79e4e
Dmitry Leschik <dmitry.leschik@gmail.com>
2024-05-29 21:02:59 +0300

    add tests for get method

--- internal/services/cart_test.go
+++ internal/services/cart_test.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"database/sql"
 	"testing"
 
 	"github.com/p1mako/cart-api/internal/models"
@@ -56,6 +57,9 @@ func getDependencies() (*CartDbMocked, *CartItemServiceMocked) {
 			Id:    1,
 			Items: nil,
 		}, nil)
+	cartDb.
+		On("Load", 2).
+		Return(models.Cart{}, sql.ErrNoRows)
 	cartItemServ.
 		On("Create", models.CartItem{
 			CartId:   1,
@@ -77,12 +81,25 @@ func getDependencies() (*CartDbMocked, *CartItemServiceMocked) {
 			Quantity: 1,
 		}}, nil)
 	cartItemServ.
+		On("GetCartItems", 2).
+		Return(mock.Anything, ErrNoSuchCart{Id: 2})
+	cartItemServ.
 		On("Remove", models.CartItem{Id: 1, CartId: 1}).
 		Return(nil)
 	cartItemServ.
 		On("Remove", models.CartItem{Id: 2, CartId: 1}).
 		Return(ErrNoSuchItem{})
 	return cartDb, cartItemServ
+}
+
+func getCartService() CartManipulator {
+	cartDb, cartItemServ := getDependencies()
+
+	cartServ := CartManipulator{
+		db:       cartDb,
+		itemServ: cartItemServ,
+	}
+	return cartServ
 }
 
 var addItemTests = []struct {
@@ -127,15 +144,53 @@ var addItemTests = []struct {
 }
 
 func TestCartService_AddItem(t *testing.T) {
+	cartServ := getCartService()
+	for _, test := range addItemTests {
+		t.Run(test.name, func(t *testing.T) {
+			out1, out2 := cartServ.AddItem(test.input)
+			assert.Equal(t, out1, test.expected)
+			assert.Equal(t, out2, test.err)
+		})
+	}
+}
+
+var getTests = []struct {
+	input    int
+	expected models.Cart
+	name     string
+	err      error
+}{
+	{
+		name:  "Valid",
+		input: 1,
+		expected: models.Cart{
+			Id: 1,
+			Items: []models.CartItem{{
+				Id:       1,
+				CartId:   1,
+				Product:  "qwerty",
+				Quantity: 1,
+			}},
+		},
+		err: nil,
+	},
+	{
+		name:  "Invalid id",
+		input: 2,
+		err:   ErrNoSuchCart{Id: 2},
+	},
+}
+
+func TestCartService_Get(t *testing.T) {
 	cartDb, cartItemServ := getDependencies()
 
 	cartServ := CartManipulator{
 		db:       cartDb,
 		itemServ: cartItemServ,
 	}
-	for _, test := range addItemTests {
+	for _, test := range getTests {
 		t.Run(test.name, func(t *testing.T) {
-			out1, out2 := cartServ.AddItem(test.input)
+			out1, out2 := cartServ.Get(test.input)
 			assert.Equal(t, out1, test.expected)
 			assert.Equal(t, out2, test.err)
 		})
