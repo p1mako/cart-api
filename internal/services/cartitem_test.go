432c0adacfd3c8a77289c2cd54cae5f474a89b5a
Dmitry Leschik <dmitry.leschik@gmail.com>
2024-05-29 22:00:52 +0300

    add tests for create item

--- internal/services/cartitem_test.go
+++ internal/services/cartitem_test.go
@@ -1,7 +1,11 @@
 package services
 
 import (
+	"database/sql"
+	"testing"
+
 	"github.com/p1mako/cart-api/internal/models"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -20,4 +24,80 @@ func (db *CartItemDbMock) LoadCartItems(cart int) ([]models.CartItem, error) {
 func (db *CartItemDbMock) Remove(item models.CartItem) (int, error) {
 	args := db.Called(item)
 	return args.Int(0), args.Error(1)
+}
+
+func getCartManipulatorWithMock() *CartItemManipulator {
+	db := new(CartItemDbMock)
+	db.On("Create", []models.CartItem{{
+		CartId:   1,
+		Product:  "qqqqq",
+		Quantity: 10,
+	}}).
+		Return(
+			[]models.CartItem{{
+				Id:       1,
+				CartId:   1,
+				Product:  "qqqqq",
+				Quantity: 10,
+			}},
+			nil,
+		)
+	db.
+		On("Create", []models.CartItem{{
+			CartId:   2,
+			Product:  "qqqqq",
+			Quantity: 10,
+		}}).
+		Return(
+			[]models.CartItem{},
+			sql.ErrNoRows,
+		)
+	return &CartItemManipulator{db: db}
+}
+
+var createTests = []struct {
+	name     string
+	input    models.CartItem
+	expected models.CartItem
+	err      error
+}{
+	{
+		name: "Valid",
+		input: models.CartItem{
+			CartId:   1,
+			Product:  "qqqqq",
+			Quantity: 10,
+		},
+		expected: models.CartItem{
+			Id:       1,
+			CartId:   1,
+			Product:  "qqqqq",
+			Quantity: 10,
+		},
+	},
+	{
+		name: "Invalid cart",
+		input: models.CartItem{
+			CartId:   2,
+			Product:  "qqqqq",
+			Quantity: 10,
+		},
+		err: ErrNoSuchCart{Id: 2},
+	},
+}
+
+func TestCreate(t *testing.T) {
+	sMocked := getCartManipulatorWithMock()
+	for _, test := range createTests {
+		t.Run(test.name, func(t *testing.T) {
+			out1, out2 := sMocked.Create(test.input)
+			if !assert.ErrorIs(t, out2, test.err) {
+				assert.Equal(t, out1, test.expected)
+			}
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+
 }
